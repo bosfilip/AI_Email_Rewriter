26cbe9237b3e8395cd2323d119a9a5607a5d345a
bosfilip <bosfil1608@gmail.com>
2025-12-09 18:42:26 +0100

    Results panel UI half way done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './Header.jsx'
 import EditorPanel from './editorPanel.jsx'
+import ResultPanel from './resultPanel'
 
 function App() {
   return(
   <>
     <Header/>
     <EditorPanel/>
+    <ResultPanel/>
   </>
   )
 }
